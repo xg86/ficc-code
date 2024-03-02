9f4458fce9c1f5078b5529471b6fb6084505deea
guogl <glen.xin.guo@gmail.com>
2024-03-02 16:12:35 +1100

    from zhanghao

---
+++ 最新代码/CurveSpread_full_rv.py
@@ -0,0 +1,246 @@
+import uuid
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+import statsmodels.tsa.stattools as tsa
+import statsmodels.api as sm
+import statsmodels.tsa.stattools as ts
+from pandas import DataFrame
+from sklearn import preprocessing
+from sklearn.cluster import DBSCAN
+from sklearn.decomposition import PCA
+import seaborn as sn
+from datetime import datetime
+import datetime as dt
+from timeit import default_timer as timer
+from dateutil.relativedelta import relativedelta
+
+from mcpParamCurve import *
+
+pd.options.mode.chained_assignment = None
+import warnings
+
+warnings.filterwarnings("ignore")
+
+start = '2021-01-4 00:00:00'
+cutoff = "2023-11-1 00:00:00"
+end = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+# bond_codes = ["210203.IB", "190215.IB", "210205.IB", "210210.IB", "220205.IB",
+#             "220210.IB", "220215.IB", "220220.IB", "230205.IB", "210220.IB"]
+src_files = ['每日-进出口债收盘历史.csv', '每日-国开债收盘历史.csv', '每日-农发债收盘历史.csv', '每日-国债收盘历史.csv']
+curve_categories = ['03', '02', '04', '00']
+# src_files = ['每日-进出口债收盘历史.csv']
+
+spread_file = 'E://meridian//债券//信号统计//NSS信号.xlsx'
+spread_df = pd.read_excel(spread_file, sheet_name='bid-ask-spread')
+
+static_file = 'D://git//strategy-repos-master//butterfly//nss-data//WIND_BOND.xlsx'
+static_df = pd.read_excel(static_file, sheet_name='Sheet1')
+
+
+def get_half_life2(z_array: pd.Series):
+    z_lag = np.roll(z_array, 1)
+    z_lag[0] = 0
+    z_ret = z_array - z_lag
+    z_ret[0] = 0
+    # adds intercept terms to X variable for regression
+    z_lag2 = sm.add_constant(z_lag)
+    model = sm.OLS(z_ret, z_lag2)
+    res = model.fit()
+    halflife = -np.log(2) / res.params[1]
+    # print('Halflife = ', halflife)
+    return halflife
+
+
+def get_half_life(data: pd.Series):
+    if (len(data) < 2):
+        return -1
+    data_lag = data.shift(1)
+    Y = data[1:] - data_lag[1:]
+    X = sm.add_constant(data_lag[1:])  # Adds a column of ones to an array
+    model = sm.OLS(Y, X)
+    res = model.fit()
+    np_log = -np.log(2)
+    if (len(res.params) < 2):
+        return -1
+    res_params = res.params.iloc[1]
+    param_1 = np_log / res_params
+    halflife = round(param_1, 0)
+    return halflife
+
+
+def stationary_test(data2: pd.Series):
+    # compute ADF test statistic
+    adf_ = tsa.adfuller(data2, maxlag=1)
+    # print('adf', adf_[0])
+    # print('p-value', adf_[1])
+    # print('T values', adf_[4])
+    return adf_[1] < 0.1
+
+
+def getYield(df1: DataFrame, col1: str, col2: str):
+    if df1[col2] >= 1:
+        return df1[col1]
+
+
+def getPnl(df2: DataFrame, col1: str, col2: str, multiplier: int, isYield: bool):
+    # if df['Buy'] == 1 and df['SellYield'] != None and df['BuyYield'] > 0 and df['SellYield'] > 0 :
+    if df2['Sell'] == 1 and df2[col1] != None and df2[col1] > 0 and df2[col2] > 0:
+        if isYield:
+            return (df2[col1] - df2[col2]) * multiplier
+        return (df2[col2] - df2[col1]) * multiplier
+
+
+# one buy one sell
+def calculatePnl(df3: DataFrame):
+    df3['BuyYield'] = df3.apply(getYield, args=('Yield', 'Buy'), axis=1)
+    df3['SellYield'] = df3.apply(getYield, args=('Yield', 'Sell'), axis=1)
+    # df['BuyDirtyPrice'] = df.apply(getYield, args=('ValuationDirtyPrice', 'Buy'), axis=1)
+    # df['SellDirtyPrice'] = df.apply(getYield, args=('ValuationDirtyPrice', 'Sell'), axis=1)
+    # if len(df[df['Sell'] == 1]) > 1:
+    #     df['SellYield'].fillna(method='bfill', inplace=True)
+    b_yield = -1
+    b_yield_curveSpread = -1
+    for index, row in df3.iterrows():
+        # print(row["Name"], row["Age"])
+        if row['Buy'] == 1 and b_yield < 0:
+            b_yield = row['BuyYield']
+            b_yield_curveSpread = row['curveSpread']
+        elif row['Sell'] == 1 and b_yield > 0:
+            df3.at[index, 'BuyYield'] = b_yield
+            df3.at[index, 'b_yield_curveSpread'] = b_yield_curveSpread
+            b_yield = -1
+            b_yield_curveSpread = -1
+
+    df3['PnLYield'] = df3.apply(getPnl, args=('BuyYield', 'SellYield', 100, True), axis=1)
+    # df['PnLPrice'] = df.apply(getPnl, args=('BuyDirtyPrice', 'SellDirtyPrice', 100*1000, False), axis=1)
+    return df3['PnLYield'].sum()  # , df['PnLPrice'].sum()
+
+
+def check_spread(spread_trades: pd.Series, spread_sigal: pd.Series, code: str, pnlY: float, left_year, side: str,
+                 trades_time: pd.Series):
+    code_df = spread_df[(spread_df.bond_code == code)]
+    b_a_spread = 0
+    if len(code_df) > 0:
+        b_a_spread = code_df['spread'].tolist()[0]
+    min_val = spread_trades.min()
+    for indx, val in spread_sigal.items():
+        if val > min_val:
+            # print("diff yield {0}, bid-ask spread {1} for code {2}".format (val, b_a_spread, code))
+            print(
+                "%%%%%%%%%%%% {5} for-code {0} on {7} curveSpread {8}, total-PnL {1}, diff-curveSpread {2}, bid-ask-spread {3}, zs-b-a_sprd {6} left {4} "
+                .format(code, pnlY, val - min_val, b_a_spread, left_year, side, val - min_val - b_a_spread,
+                        trades_time.max(), spread_sigal.max()))
+            return True
+    return False
+    pass
+
+
+def get_left_year(mat_date: datetime):
+    now = datetime.now()
+    return relativedelta(mat_date, now)
+    pass
+
+
+def cal_spread(newdf: DataFrame, yield_str: str, date_str: str, maturity_date: datetime, coupon: float, freq: int,
+               category: str):
+    return get_zspread(newdf[yield_str] / 100, newdf[date_str], maturity_date, coupon / 100, freq, category)
+
+
+def get_rv_signal(src_file: str, category: str):
+    print("starting reading ")
+    request_file = 'D://git//strategy-repos-master//butterfly//nss-data//' + src_file
+    src_df = pd.read_csv(request_file)
+    src_df['Code'] = src_df['代码']
+    codes = src_df.Code.unique()
+
+    filename = str(uuid.uuid4().hex) + "_CurveSpread_full_" + src_file + ".xlsx"
+    writer = pd.ExcelWriter("D://git//strategy-repos-master//butterfly//nss-data-proc//" + filename)
+    print("codes length ", len(codes))
+    for code in codes:
+        if (len(code) <= 9 and code.endswith('IB')):
+            newdf = src_df[(src_df.Code == code)]
+            newdf['TradeTime'] = pd.to_datetime(newdf['时间'])
+            newdf['Yield'] = newdf['收益率']
+            # newdf['Volumn'] = newdf['成交量']
+            # newdf['TranNum'] = newdf['成交笔数']
+            newdf = newdf[((newdf['成交量'] > 0) | (newdf['成交笔数'] > 0)) & (newdf['收益率'] > 0)]
+            newdf.set_index('时间', inplace=True)
+
+            # print("code is  ", code)
+            newdf = newdf.tail(380)
+            # if (len(newdf) <= 100):
+            #    continue
+            #
+            newdf = newdf[newdf.TradeTime.between(start, end)]
+            if (len(newdf) < 1):
+                continue
+            # print('bond code  = ', code)
+            code_df = static_df[(static_df.CODE == code)]
+            if (len(code_df) < 1):
+                continue
+            maturityDates = code_df['MATURITYDATE']
+            maturity_date = pd.to_datetime(maturityDates.iloc[0])
+            coupon = code_df['COUPONRATE'].iloc[0]
+            freq = code_df['INTERESTFREQUENCY'].iloc[0]
+            # if(code == '092203005.IB'):
+            #    print('bond code  = ', code)
+            newdf['curveSpread'] = newdf.apply(cal_spread,
+                                               args=('Yield', 'TradeTime', maturity_date, coupon, freq, category),
+                                               axis=1)
+            halflife_Yield = get_half_life(newdf["Yield"])
+
+            if (halflife_Yield > 2):
+                window_size = int(np.rint(halflife_Yield))
+                # newdf['AbsDiffZero'] = np.abs(newdf["NssZero"]-newdf["Zero"])*10000
+                newdf['ave_' + str(window_size)] = newdf['curveSpread'].rolling(window=int(window_size),
+                                                                            center=False).mean()
+                newdf['std_' + str(window_size)] = newdf['curveSpread'].rolling(window=int(window_size), center=False).std(
+                    ddof=0)
+                newdf['zscore_' + str(window_size)] = (newdf['curveSpread'] - newdf['ave_' + str(window_size)]) / newdf[
+                    'std_' + str(window_size)]
+
+                newdf['Buy'] = np.where(newdf['zscore_' + str(window_size)] >= 1, 1, 0)
+                newdf['Sell'] = np.where(newdf['zscore_' + str(window_size)] <= -1, 1, 0)
+                #newdf['Sell'] = np.where(newdf['zscore_' + str(window_size)] <= -1, np.where(newdf['std_' + str(window_size)] > 0.5, 1, 0), 0)
+
+                # get_zspread(newdf['Yield'], newdf['Date'], newdf['MaturityDate'], newdf['Coupon'])
+
+                pnlY = calculatePnl(newdf)
+                print(">>>>>>>>>>>> pnl yield %s, for code %s" % (pnlY, code))
+                skip_df = newdf[newdf.TradeTime.between(cutoff, end)]
+                # skip_df = newdf.loc[cutoff: cutoff]
+                if (len(skip_df) <= 2):
+                    continue
+                tmp20 = newdf.tail(20)
+                left_year = get_left_year(maturity_date)
+                print("$$$$$$$$$ for code {0}, mat {4} min curveSpread {1}, Max curveSpread {2}, min-max spread {3}, left {5}"
+                      .format(code, tmp20['curveSpread'].min(), tmp20['curveSpread'].max(),
+                              tmp20['curveSpread'].max() - tmp20['curveSpread'].min(), maturity_date, left_year))
+                newdf.to_excel(writer, sheet_name=code)
+                tmp = newdf.tail(2)
+                p_val = stationary_test(newdf['curveSpread'])
+                if (len(tmp[tmp['Buy'] == 1]) > 0 or len(
+                        tmp[tmp['Sell'] == 1]) > 0) and pnlY > 0 and p_val and code.startswith('2'):
+                    tradedf = newdf[(newdf.PnLYield > 0)]
+                    if (len(tradedf) <= 2):
+                        continue
+                    check_spread(tradedf['b_yield_curveSpread'], tmp['curveSpread'], code, pnlY, left_year,
+                                 'Buy' if len(tmp[tmp['Buy'] == 1]) > 0 else 'Sell', tmp['TradeTime'])
+
+                # if len(tmp[tmp['Sell'] == 1]) > 0 :
+                #    print("############ Sell trading sigal for code is  ", code)
+                # print("done for code is  ", code)
+
+    writer.close()
+    print("ALl done ", src_file)
+
+
+# for src_file in src_files:
+#    get_rv_signal(src_file)
+
+for index in range(len(src_files)):
+    get_rv_signal(src_files[index], curve_categories[index])
